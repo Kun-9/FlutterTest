04f02da52c0e8ce2ebc59ea8c5bbbd2b84142a93
kun <sus5330@icloud.com>
2021-12-31 15:50:40 +0900

    application drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,148 +3,99 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'KUN',
-      home: Grade(),
+      title: 'AppBar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: Text('HELLO'),
-        backgroundColor: Colors.grey[400],
+        title: Text('Appbar icon menu'),
         centerTitle: true,
         elevation: 0,
+        actions: [
+          IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                print('shopping click');
+              }),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('search');
+            },
+          ),
+        ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
                 backgroundImage: AssetImage('assets/image1.jpeg'),
-                radius: 60.0,
               ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.black87,
-                letterSpacing: 2.0,
+              accountName: Text('kun'),
+              accountEmail: Text('st_@naver.com'),
+              onDetailsPressed: () {
+                print("hello");
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0)),
               ),
+              // otherAccountsPictures: const [
+              //   CircleAvatar(
+              //     backgroundImage: AssetImage('assets/image2.jpeg'),
+              //   ),
+              //   CircleAvatar(
+              //     backgroundImage: AssetImage('assets/image2.jpeg'),
+              //   ),
+              // ],
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'KUN',
-              style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'KUN POWER LEVEL',
-              style: TextStyle(
-                color: Colors.black87,
-                letterSpacing: 2.0,
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
               ),
+              title: Text('HOME'),
+              onTap: () {
+                print('click home');
+              },
+              trailing: Icon(Icons.add),
             ),
-            SizedBox(
-              height: 10.0,
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text('SETTINGS'),
+              onTap: () {
+                print('click setting');
+              },
+              trailing: Icon(Icons.add),
             ),
-            Text(
-              '14',
-              style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: const <Widget>[
-                        Icon(Icons.check_circle_outline),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text(
-                          'using lightsaber',
-                          style: TextStyle(
-                            fontSize: 16,
-                            letterSpacing: 1,
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: const <Widget>[
-                        Icon(Icons.check_circle_outline),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text(
-                          'face hero tatoo',
-                          style: TextStyle(
-                            fontSize: 16,
-                            letterSpacing: 1,
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: const <Widget>[
-                        Icon(Icons.check_circle_outline),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text(
-                          'fire flames',
-                          style: TextStyle(
-                            fontSize: 16,
-                            letterSpacing: 1,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: 50,
-                ),
-                Center(
-                    child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/image2.jpeg'),
-                  radius: 40.0,
-                )),
-              ],
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: () {
+                print('click qeustion');
+              },
+              trailing: Icon(Icons.add),
             ),
           ],
         ),
